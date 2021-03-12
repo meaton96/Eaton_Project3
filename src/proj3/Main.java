3a880ab60985f74557fab63a1e7b4b1a7bea13a5
Mike <lammara96@gmail.com>
2021-03-12 16:42:22 -0500

    closer, will get up to N solutions, need to figure how to get multiple solutions without moving first queen column on 1st row

--- src/proj3/Main.java
+++ src/proj3/Main.java
@@ -2,6 +2,8 @@ package proj3;
 
 import adt.LinkedStack;
 
+import java.util.EmptyStackException;
+
 public class Main {
 
 
@@ -23,44 +25,66 @@ public class Main {
         }
 
         */
-        for (int y = 1; y <= 2; y++) {
-            runScenario(y, boardSize);
-
-            //System.out.println("Number of solutions: " + numSolutions);
-            int size  = queenStack.size();
-            for (int x = 0; x < size; x++) {
-                System.out.print(queenStack.pop() + "\t");
-            }
-            System.out.println();
-        }
+        runScenario(8);
     }
 
-    public static int runScenario(int start, int boardSize) {
-        //queenStack.clear();
-        queenStack.push(new Queen(start,1));
+    public static void runScenario(int boardSize) {
+        int start = 1;
         int startingCol = start;
-        int startingRow = 1;
+        int startingRow = 2;
         boolean wasRemoved = false;
-        while (queenStack.size() < boardSize) {
-            if (runSingleScenario(boardSize, startingCol, startingRow, wasRemoved)){
-                startingRow = queenStack.peek().getBoardPosition().getRow();
-                startingCol = 1;
-                wasRemoved = false;
-                continue;
+        int solCount = 0;
+        while (true) {
+            queenStack.push(new Queen(start, 1));
+
+            while (queenStack.size() < boardSize) {
+              //  System.out.print("stack:\t");
+                /*for (int x = 0; x < queenStack.size(); x++) {
+                    System.out.print(queenStack.itemAt(x) + "\t");
+                }*/
+              //  System.out.println();
+                if (runSingleScenario(boardSize, startingCol, startingRow, wasRemoved)) {
+                    startingRow = queenStack.peek().getBoardPosition().getRow();
+                    startingCol = 1;
+                    wasRemoved = false;
+                    continue;
+                }
+                try {
+                   // System.out.println("Removed queen at: " + queenStack.peek());
+                    startingCol = queenStack.peek()
+                            .getBoardPosition()
+                            .getCol() + 1;
+                    startingRow = queenStack.pop()
+                            .getBoardPosition()
+                            .getRow();
+
+                    wasRemoved = true;
+                } catch (EmptyStackException e) {
+                    System.out.println("Solution count: " + solCount);
+                    return;
+                }
             }
-            startingCol = queenStack.peek()
-                    .getBoardPosition()
-                    .getCol() + 1;
-            startingRow = queenStack.pop()
-                    .getBoardPosition()
-                    .getRow();
-            wasRemoved = true;
+            for (int x = 1; x < boardSize; x++) {
+                if (queenStack.itemAt(x).getBoardPosition().getRow() == 1) {
+                    start = queenStack.itemAt(x).getBoardPosition().getCol();
+                    break;
+                }
+            }
+            System.out.println("Solution: " + ++solCount);
+            for (int x = 0; x < boardSize; x++) {
+                System.out.print(queenStack.pop() + "\t");
+            }
+            System.out.println("\n");
 
             //System.out.println("removing queen: " + queenStack.peek());
         }
-        return 1;
+        //System.out.println("Solution count: " + solCount);
     }
+
     public static boolean runSingleScenario(int boardSize, int startingCol, int startingRow, boolean wasRemoved) {
+        if (startingCol > boardSize || startingRow > boardSize)
+            return false;
+
         Queen queen = new Queen(startingCol, startingRow);
         /* System.out.println("Stack: ");
         for (int x = 0; x < queenStack.size(); x++) {
@@ -68,7 +92,7 @@ public class Main {
         }
 
          */
-        //System.out.println("created new queen: " + queen);
+      //  System.out.println("created new queen: " + queen);
 
 
         boolean wasAdded = false;
@@ -79,7 +103,7 @@ public class Main {
 
             boolean isConflict = false;
             for (int x = 0; x < queenStack.size(); x++) {
-                //System.out.println("Trying queen: " + queen + " vs " + queenStack.itemAt(x));
+                // System.out.println("Trying queen: " + queen + " vs " + queenStack.itemAt(x));
 
                 if (queen.conflicts(queenStack.itemAt(x))) {
                     isConflict = true;
@@ -89,6 +113,7 @@ public class Main {
             if (!isConflict) {
                 wasAdded = true;
                 queenStack.push(queen);
+               // System.out.println("added queen: " + queenStack.peek());
             }
             if (!wasAdded)
                 queen.getBoardPosition().incrementCol();
